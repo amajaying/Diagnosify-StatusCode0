230b330375733771500ba221cd7ab15f7195ffe6
Ajay Khatri <amajaying@gmail.com>
2023-08-13 11:19:34 +0530

    reverted

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const { spawn } = require("child_process");
-const fs = require("fs");
+const express = require('express');
+const { exec } = require('child_process');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
 
+<<<<<<< HEAD
 app.post("/predict", async(req, res) => {
     try {
         const symptomIndices = req.body.symptomIndices;
@@ -39,9 +37,40 @@ app.post("/predict", async(req, res) => {
     } catch (error) {
         console.error("Error:", error);
         res.status(500).json({ error: "An error occurred" });
+=======
+app.post('/predict', (req, res) => {
+    const pythonScriptPath = '../model/predict.py';
+    const indices = req.body.indices;
+    const symptoms = 375;
+    //convert it to binary array
+    const data = [];
+    for (let i = 0; i < 375; i++) {
+        if (indices.includes(i)) {
+            data.push(1);
+        } else {
+            data.push(0);
+        }
+>>>>>>> parent of 11778be (Backend Optimized)
     }
+
+    const data2 = [data];
+
+    //pass the data to python script and execute it 
+    const spawn = require("child_process").spawn;
+    const pythonProcess = spawn('python', ["../model/predict.py", JSON.stringify(data2)]);
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(data.toString());
+        res.json(data.toString());
+    });
+
+
+
+
+
+
+
 });
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
