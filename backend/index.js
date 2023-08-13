11778be848a9b222c40dd2aaf206547b59822bfa
Ajay Khatri <amajaying@gmail.com>
2023-08-13 10:26:25 +0530

    Backend Optimized

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,47 @@
-const express = require('express');
-const { exec } = require('child_process');
+const express = require("express");
+const cors = require("cors");
+const { spawn } = require("child_process");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
-app.post('/predict', (req, res) => {
-    const pythonScriptPath = '../model/predict.py';
-    const indices = req.body.indices;
-    const symptoms = 375;
-    //convert it to binary array
-    const data = [];
-    for (let i = 0; i < 375; i++) {
-        if (indices.includes(i)) {
-            data.push(1);
-        } else {
-            data.push(0);
-        }
+app.post("/predict", async(req, res) => {
+    try {
+        const symptomIndices = req.body.symptomIndices;
+        const totalSymptoms = 375;
+        const inputDat = Array.from({ length: totalSymptoms }, (_, i) =>
+            symptomIndices.includes(i) ? 1 : 0
+        );
+        const inputData = [inputDat];
+
+        // Define the correct outputFilePath
+        const outputFilePath = "./output.json";
+
+        fs.readFile(outputFilePath, "utf8", (err, data) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: "An error occurred" });
+                return;
+            }
+
+            try {
+                const predictionData = JSON.parse(data);
+                res.json(predictionData);
+            } catch (parseError) {
+                console.error("Error parsing JSON:", parseError);
+                res.status(500).json({ error: "An error occurred" });
+            }
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "An error occurred" });
     }
-
-    const data2 = [data];
-
-    //pass the data to python script and execute it 
-    const spawn = require("child_process").spawn;
-    const pythonProcess = spawn('python', ["../model/predict.py", JSON.stringify(data2)]);
-    pythonProcess.stdout.on('data', (data) => {
-        console.log(data.toString());
-        res.json(data.toString());
-    });
-
-
-
-
-
-
-
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
