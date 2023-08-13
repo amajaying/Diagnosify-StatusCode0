14bc72dc41e84dd9b83b1ff1ea081f3fee727dc9
Ajay Khatri <amajaying@gmail.com>
2023-08-13 14:40:39 +0530

    Final Submission

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,46 @@
-const express = require("express");
-const cors = require("cors");
-const { spawn } = require("child_process");
-const fs = require("fs");
+const express = require('express');
+const { python } = require('python-shell');
+const fs = require('fs');
 
 const app = express();
-const port = 3000;
 
-app.use(express.json());
-app.use(cors());
+app.post('/predict', async(req, res) => {
 
-app.post("/predict", async(req, res) => {
-    try {
-        const symptomIndices = req.body.symptomIndices;
-        const totalSymptoms = 375;
-        const inputDat = Array.from({ length: totalSymptoms }, (_, i) =>
-            symptomIndices.includes(i) ? 1 : 0
-        );
-        const inputData = [inputDat];
 
-        // Define the correct outputFilePath
-        const outputFilePath = "./output.json";
-
-        fs.readFile(outputFilePath, "utf8", (err, data) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ error: "An error occurred" });
-                return;
-            }
-
-            try {
-                const predictionData = JSON.parse(data);
-                res.json(predictionData);
-            } catch (parseError) {
-                console.error("Error parsing JSON:", parseError);
-                res.status(500).json({ error: "An error occurred" });
-            }
-        });
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "An error occurred" });
+    data = req.body.systemIndices;
+    const symptomNo = 375;
+    //1 if present in data, 0 if not present
+    for (var i = 0; i < symptomNo; i++) {
+        if (data[i] == undefined) {
+            data[i] = 0;
+        } else {
+            data[i] = 1;
+        }
     }
+    //write data to file
+    fs.writeFile('../model/input.json', JSON.stringify(data), function(err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+    //execute python script expect no result
+    python.run('../model/model.py', null, function(err) {
+        if (err) throw err;
+        console.log('finished');
+    });
+
+    //read result from file
+    var result = fs.readFileSync('../model/output.json', 'utf8');
+
+
+    //send result to frontend
+    res.send(result.toString());
+
+
+
+
+
 });
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
